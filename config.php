2986d02be99fd26950eb145757e4975f552da53a
isra00 <isra00@gmail.com>
2014-12-25 12:30:51 +0300

    Separar init y config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NeoTransposer;
+
+define('SOFTWARE_NAME', 'Neo-Transposer');
+
+define('DB_HOST',		'localhost');
+define('DB_USER',		'root');
+define('DB_PASSWORD',	'root');
+define('DB_DATABASE',	'transposer');
+define('DB_CHARSET',	'utf8');
+
+define('ANALYTICS_ID',	'UA-57809429-1');
+
+define('DEFAULT_BOOK', '1');
+define('DEFAULT_CHORD_PRINTER', 'English');
--- init.php
+++ init.php
@@ -2,26 +2,13 @@
 
 namespace NeoTransposer;
 
-define('SOFTWARE_NAME', 'Neo-Transposer');
-
-define('DB_HOST',		'localhost');
-define('DB_USER',		'root');
-define('DB_PASSWORD',	'root');
-define('DB_DATABASE',	'transposer');
-define('DB_CHARSET',	'utf8');
-
-define('ANALYTICS_ID',	'UA-57809429-1');
-
-define('DEFAULT_BOOK', '1');
-define('DEFAULT_CHORD_PRINTER', 'English');
+include 'config.php';
 
 $GLOBALS['chord_printers'] = array(
 	'English' => 'English (F#m, Bb7)',
 	'Swahili' => 'Swahili (Fd-, Eb7)',
 	'Spanish' => 'Española (Fa#-, Sib7)',
 );
-
-/******************************************************************************/
 
 define('START_TIME', microtime(true));
 
