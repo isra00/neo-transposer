13c32b6cbf7460b21bb3c3044d734bf55047f52f
isra00 <isra00@gmail.com>
2015-01-24 22:37:14 +0300

    Estética de código config.php

--- config.php
+++ config.php
@@ -1,28 +1,26 @@
 <?php
 
-$config = array();
+return array(
 
-$config['db'] = array(
-	'host' => 'localhost',
-	'user' => 'root',
-	'password' => 'root',
-	'database' => 'transposer',
-	'charset' => 'utf8'
+	'db' => array(
+		'host'		=> 'localhost',
+		'user'		=> 'root',
+		'password'	=> 'root',
+		'database'	=> 'transposer',
+		'charset'	=> 'utf8'
+	),
+
+	// URLs for the Book controller. Every book must have an entry here!
+	'book_url' => array(
+		1 => '/nyimbo-njia-neokatekumenato',
+		2 => '/cantos-camino-neocatecumenal',
+	),
+
+	'software_name'		=> 'Neo-Transposer',
+	'analytics_id'		=> 'UA-57809429-1',
+
+	'templates_dir'		=> __DIR__ . '/templates',
+	'translation_file'	=> __DIR__ . '/translations.php',
+
+	'debug'				=> false,
 );
-$config['templates_dir'] = __DIR__ . '/templates';
-$config['translation_file'] = __DIR__ . '/translations.php';
-
-$config['software_name'] = 'Neo-Transposer';
-
-$config['analytics_id'] = 'UA-57809429-1';
-$config['default_book'] = '1';
-$config['default_chord_printer'] = 'English';
-
-$config['book_url'] = array(
-	1 => '/nyimbo-njia-neokatekumenato',
-	2 => '/cantos-camino-neocatecumenal'
-);
-
-$config['debug'] = false;
-
-return $config;
