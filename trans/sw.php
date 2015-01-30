35c03ff1bb18743dfac710bca728419441f2b038
isra00 <isra00@gmail.com>
2015-01-30 11:42:39 +0300

    Primera versión de la traducción SW, faltan muchas cadenas

---
+++ trans/sw.php
@@ -0,0 +1,102 @@
+<?php
+
+return array(
+
+	//General
+	'Developed in Tanzania. Contact: %email%'
+	=> 'Imetengenezwa Tanzania. Mawasiliano: %email%',
+
+	'Log-out'
+	=> 'Ondoka',
+/*
+	'Neo-Transposer automatically transposes the songs of the Neocatechumenal Way for you, so they fit your voice perfectly.'
+	=> 'Neo-Transposer transporta automáticamente los cantos del Camino por ti, para que encajen perfectamente con tu voz.',
+*/
+	//Login
+	'Log-in'
+	=> 'Ingia',
+
+	'Welcome to %software%'
+	=> 'Karibu kwenye %software%',
+
+	'Please, type your e-mail:'
+	=> 'Weka anwani yako ya barua pepe:',
+
+	'Enter'
+	=> 'Ingia',
+
+	'That e-mail doesn\'t look good. Please, re-type it.'
+	=> 'Anwani ya barua pepe uliyoingiza si sahihi.',
+
+	//User settings
+/*	'Settings'
+	=> 'Configuración',
+
+	'This software analyses the songs and your voice, giving you the perfect transposition for each song, according to your voice. But to do so, first I need to know your voice.'
+	=> 'Este software analiza los cantos y tu voz, calculando la transposición perfecta para cada canto, de acuerdo a tu voz. Pero para hacer eso, necesito saber una cosa sobre tu voz.',
+
+	'Which is the lowest note that you can sing? And the highest one?'
+	=> '¿Cuál es la nota más grave que puedes cantar? ¿Y la más aguda?',
+
+	'Lowest:'
+	=> 'Más grave:',
+	
+	'Highest:'
+	=> 'Más aguda:',
+	
+	'octave'
+	=> 'octava',
+*/
+	'Which songbook do you want to transpose?'
+	=> 'Chagua lugha ya kitabu cha nyimbo:',
+	
+	'We are ready!'
+	=> 'Tayari!',
+
+	//Book
+	'Songs of the Neocatechumenal Way in %lang%'
+	=> 'Nyimbo za Njia ya Neokatekumenato kwa %lang%',
+
+	'Change book'
+	=> 'Lugha nyingine',
+
+	//TransposeSong
+	'%song% (Neocatechumenal Way)'
+	=> '%song% (Njia ya Neokatekumenato)',
+/*
+	'Transpose the chords of &quot;%song%&quot; (song of the Neocatechumenal Way) automatically so you can sing it without stress!'
+	=> '¡Transporta automáticamente los acordes de &quot;%song%&quot; (canto del Camino Neocatecumenal) para que puedas cantar sin estrés!',
+*/
+	'Your voice:'
+	=> 'Sauti yako:',
+	
+	'Change'
+	=> 'Geuza',
+	
+/*	'These two transpositions match your voice (they are equivalent). The first one has easier chords:'
+	=> 'Estas dos transposiciones encajan con tu voz (son equivalentes). La primera tiene acordes más fáciles:',
+
+	'with capo %n%'
+	=> 'cejilla %n%º traste',
+	
+	'(no capo)'
+	=> '(sin cejilla)',
+	
+	'(same chords as in the book)'
+	=> '(mismos acordes que en el libro)',
+	
+	'This other transposition is a bit %difference%, but it has easier chords and may also fit your voice:'
+	=> 'Esta otra transposición es un poco %difference%, pero tiene acordes más fáciles y puede venir bien a tu voz:',
+	
+	'higher'
+	=> 'más aguda',
+
+	'lower'
+	=> 'más grave',
+*/
+	'Original chords:'
+	=> 'Kwenye kitabu:',
+	
+	'Transposed:'
+	=> 'Kwa kubadili ufunguo:', //REVISAR
+);
