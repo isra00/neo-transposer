8ea48ab5f7bade8088bebc24cb0b6f488a48a62a
isra00 <isra00@gmail.com>
2015-04-08 23:58:16 +0300

    Fix bug: user was not redirected to the proper book in 1st usage

--- src/NeoTransposer/Controllers/UserVoice.php
+++ src/NeoTransposer/Controllers/UserVoice.php
@@ -10,12 +10,26 @@ class UserVoice
 	{
 		$nc = new \NeoTransposer\NotesCalculator;
 
+		$redirect = $request->get('redirect');
+
+		//First usage: user is redirected to the book in their language
+		if (!$redirect)
+		{
+			foreach ($app['books'] as $book)
+			{
+				if ($book['locale'] == $app['locale'])
+				{
+					$redirect = $app['url_generator']->generate('book_' . $book['id_book']);
+				}
+			}
+		}
+
 		return $app->render('user_voice.tpl', array(
 			'page_title'			=> $app->trans('Your voice'),
 			'scale'					=> $nc->numbered_scale,
 			'accoustic_scale'		=> $nc->accoustic_scale,
 			'current_notation'		=> $app['neoconfig']['languages'][$app['locale']]['notation'],
-			'redirect'				=> $request->get('redirect'),
+			'redirect'				=> $redirect
 		));
 	}
 }
