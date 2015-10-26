934e7835b3c9a120f41c8ad6da4bbfad04084ac5
isra00 <isra00@gmail.com>
2015-10-26 22:14:47 +0100

    Automatically unzip and configure updated DB

--- check_mmdb_updates.php
+++ check_mmdb_updates.php
@@ -1,10 +1,12 @@
 <?php
 
-$neoconfig = include 'config.php';
+$config_file = 'config.php';
 
+$neoconfig = include $config_file;
+
+//Download last version's MD5
 $last_md5_gzip = file_get_contents("http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.md5");
 $tmp_file = tempnam(sys_get_temp_dir(), 'MMDB-check-updates');
-
 file_put_contents($tmp_file, $last_md5_gzip);
 $last_md5 = file_get_contents($tmp_file);
 
@@ -15,3 +17,13 @@ if ($last_md5 == $neoconfig['mmdb'])
 
 echo "Update found! Downloading last version $last_md5...\n";
 exec("wget -q http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz -O $last_md5.mmdb.gz");
+exec("gzip -df $last_md5.mmdb.gz");
+
+//Update config.php
+file_put_contents(
+	$config_file, 
+	str_replace($neoconfig['mmdb'], $last_md5, file_get_contents($config_file))
+);
+
+//Remove old DB
+unlink($neoconfig['mmdb'] . '.mmdb');
