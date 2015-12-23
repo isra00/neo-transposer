f18617c0dafad124cd26f6a3d77f35e7b3e155b3
isra00@gmail.com <isra00@gmail.com>
2015-12-23 20:51:27 +0300

    Better Swahili translation

--- src/NeoTransposer/Controllers/UserBook.php
+++ src/NeoTransposer/Controllers/UserBook.php
@@ -3,15 +3,14 @@
 namespace NeoTransposer\Controllers;
 
 /**
- * Menu for choosing Book. It is important to remember the book so that the next
- * time the user logs in, he/she will be redirected to it.
+ * Menu for choosing Book. Just that.
  */
 class UserBook
 {
 	public function get(\NeoTransposer\NeoApp $app)
 	{
 		return $app->render('user_book.twig', array(
-			'page_title' => $app->trans('Select language'),
+			'page_title' => $app->trans('Choose language'),
 		));
 	}
 }
