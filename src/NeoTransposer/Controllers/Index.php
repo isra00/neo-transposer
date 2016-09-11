35bd943875994689066c86fdb91dc30d7b2b6074
isra00@gmail.com <isra00@gmail.com>
2016-09-11 17:09:56 +0200

    New mechanism for Domain migration

--- src/NeoTransposer/Controllers/Index.php
+++ src/NeoTransposer/Controllers/Index.php
@@ -28,9 +28,40 @@ class Index
 			));
 		}
 
-		return $app->redirect($app['url_generator']->generate(
-			'login', 
-			array('_locale' => $app['locale'])
-		));
+		/* 
+		 * New Domain 2016 redirections: the general policy is to redirect
+		 * everything to the new domain with 301. BUT since we have a lot of
+		 * direct access users, we want to show them a pop-up (in the new domain)
+		 * alerting them about the change. So if we detect no referer, we
+		 * redirect adding the param ?newdomain that will trigger the pop-up.
+		 * This is done in .htaccess. But there is still a little problem: home
+		 * page / redirects to language-based login page making us lose the 
+		 * referer. We solve this here, by redirecting directly to the new
+		 * domain. If we are in the new domain, do the normal stuff.
+		 */
+
+		$redirectUrl = '';
+		$params = array('_locale' => $app['locale']);
+
+		//If we are in the old domain, redirect to the new one
+		if ($req->getHost() != $app['neoconfig']['new_domain'])
+		{
+			$redirectUrl = $req->getScheme() . '://' . $app['neoconfig']['new_domain'];
+
+			if (empty($req->headers->get('referer')))
+			{
+				$params['newdomain'] = true;
+			}
+
+			//We track the referer in any case for analytics
+			$params['referer'] = $req->headers->get('referer');
+		}
+
+		$redirectUrl .= $app['url_generator']->generate(
+			'login',
+			$params
+		);
+
+		return $app->redirect($redirectUrl);
 	}
 }
--- trans/sw.php
+++ trans/sw.php
@@ -43,14 +43,14 @@ return array(
 	/*'This website is a personal initiative of a member of the Neocatechumenal Way, but it does not represent officially the Neocatechumenal Way neither its responsibles.'
 	=> 'Esta página web es iniciativa personal de un miembro del Camino Neocatecumenal pero NO representa oficialmente ni está afiliada de ningún modo al Camino Neocatecumenal o a sus responsables. El libro de cantos “Resucitó” es &copy; Kiko Argüello 2014.',*/
 
-	'Neo-Transposer changes its address to: <strong><a href="http://%domain%">%domain%</a></strong>'
-	=> 'Neo-Transposer inahama kwenda: <strong><a href="http://%domain%">%domain%</a></strong>',
+	'Neo-Transposer has moved to <strong>%domain%</strong>'
+	=> 'Neo-Transposer imehama kwenda: <strong>%domain%</strong>',
 
-	'If you have a bookmark, please update it.<br>This address will not work after November 6<sup>th</sup>.'
-	=> 'Ukitumia bookmark, tafadhali uibadilishe.<br>Anwani hii haitafanya kazi tena kuanzia tarehe 6 Novemba.',
+	'If you have a bookmark, please update it.<br>The old neo-transposer.leviticus.co.tz will not work  after November 6<sup>th</sup>.'
+	=> 'Ukitumia bookmark, tafadhali uibadilishe.<br>neo-transposer.leviticus.co.tz haitafanya kazi kuanzia tarehe 6 novemba.',
 
-	'Go to the new website'
-	=> 'Kwenda website mpya',
+	'Got it'
+	=> 'Sawa',
 
 	//User voice
 	'Your voice'
