isra00/neo-transposer

commit a4aff45f58b41601d5ecca75e7152ed3935d3c7b

Remove unused vars

src/NeoTransposer/Controllers/InsertSong.php
@@ -17,7 +17,7 @@ class InsertSong
 	 */
 	protected $db;
 
-	public function get(Request $request, \NeoTransposer\NeoApp $app, $tpl_vars=array())
+	public function get(\NeoTransposer\NeoApp $app, $tpl_vars=array())
 	{
 		$app['locale'] = 'es';
 

src/NeoTransposer/Controllers/Book.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class Book
 {
-	public function get(Request $req, \NeoTransposer\NeoApp $app, $id_book)
+	public function get(\NeoTransposer\NeoApp $app, $id_book)
 	{
 		if (empty($app['books'][$id_book]))
 		{

src/NeoTransposer/Controllers/ChordCorrectionPanel.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ChordCorrectionPanel
 {
-	public function get(Request $req, \NeoTransposer\NeoApp $app)
+	public function get(\NeoTransposer\NeoApp $app)
 	{
 		$app['locale'] = 'es';
 

src/NeoTransposer/Model/AdminTools.php
@@ -192,8 +192,6 @@ class AdminTools
 	 */
 	public function testAllTranspositions(NeoApp $app)
 	{
-		$allSongsController = new \NeoTransposer\Controllers\AllSongsReport();
-
 		/** @todo Añadir control de errores: 1) no existe el atributo app['neoconfig']['test_all_transpositions_expected'
 		2) el fichero especificado no existe */
 

commit 36949b6eb1da6b67885102bb4847bdc293149cea

Fix bug introduced in a4aff45f58b41601d5ecca75e7152ed3935d3c7b by removing the get()'s  param

src/NeoTransposer/Controllers/InsertSong.php
@@ -54,7 +54,6 @@ class InsertSong
 		$app->addNotification('success', 'Song inserted');
 
 		return $this->get(
-			$request, 
 			$app, 
 			array('id_book' => $request->get('id_book'))
 		);