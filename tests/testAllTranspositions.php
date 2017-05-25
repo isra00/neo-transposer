48fa89c717e4625656a259d174812409fa5c7f9d
isra00@gmail.com <isra00@gmail.com>
2017-05-25 16:52:50 +0300

    Code clean testAllTransposition

--- tests/testAllTranspositions.php
+++ tests/testAllTranspositions.php
@@ -2,18 +2,14 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use \Symfony\Component\HttpFoundation\Request;
-use \NeoTransposer\NeoApp;
-use \Silex\Provider;
-
-$app = new NeoApp(
+$app = new \NeoTransposer\NeoApp(
 	require __DIR__ . '/../config.php',
 	realpath(__DIR__ . '/..')
 );
 
 $adminTools = new \NeoTransposer\Model\AdminTools($app);
-$output = $adminTools->testAllTranspositions();
-$result = strpos($output, 'SUCCESSFUL');
+$output 	= $adminTools->testAllTranspositions();
+$result 	= strpos($output, 'SUCCESSFUL');
 
 /** @todo Change <strong> and <em> tags for command line colors */
 file_put_contents('php://' . ($result ? 'stdout' : 'stderr'), $output . "\n");
