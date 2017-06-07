e52be5716e17733b55873867ef9d39d36527e48e
isra00@gmail.com <isra00@gmail.com>
2017-06-07 15:50:08 +0300

    Add unit tests for AutomaticTransposer::calculatePeopleCompatible, now 100% coverage

--- tests/src/NeoTransposer/Model/AutomaticTransposerTest.php
+++ tests/src/NeoTransposer/Model/AutomaticTransposerTest.php
@@ -51,6 +51,10 @@ class AutomaticTransposerTest extends \PHPUnit\Framework\TestCase
 			$this->app['new.Transposition'] = $this->app->factory(function ($app) {
 				return new \NeoTransposer\Model\Transposition($app);
 			});
+
+			$this->app['new.PeopleCompatibleTransposition'] = $this->app->factory(function ($app) {
+				return new \NeoTransposer\Model\PeopleCompatibleTransposition($app);
+			});
 		}
 
 		return $this->app;
@@ -223,6 +227,119 @@ class AutomaticTransposerTest extends \PHPUnit\Framework\TestCase
 		$expected = new PeopleCompatibleCalculation(
 			PeopleCompatibleCalculation::WIDER_THAN_SINGER, 
 			null
+		);
+
+		$this->assertEquals(
+			$expected,
+			$this->transposer->calculatePeopleCompatible()
+		);
+	}
+
+	public function testPeopleCompatibleWiderNotAdjusted()
+	{
+		$this->transposer->setTransposerData(
+			new NotesRange('A1', 'E3'), new NotesRange('D2', 'F#3'), ['Em', 'D'], true, new NotesRange('D2', 'E3')
+		);
+
+		$expected = new PeopleCompatibleCalculation(
+			PeopleCompatibleCalculation::NOT_ADJUSTED_WIDER, 
+			null
+		);
+
+		$this->assertEquals(
+			$expected,
+			$this->transposer->calculatePeopleCompatible()
+		);
+	}
+
+	public function testPeopleCompatibleWiderAdjusted()
+	{
+		$this->transposer->setTransposerData(
+			new NotesRange('A1', 'E3'), new NotesRange('A1', 'D3'), ['Am', 'E'], true, new NotesRange('A1', 'D3')
+		);
+
+		$ppc = new \NeoTransposer\Model\PeopleCompatibleTransposition($this->app);
+		$ppc->setTranspositionData(['Am', 'E'], 2, true, 2, new NotesRange('B1', 'E3'), 1);
+		$ppc->peopleRange = new NotesRange('B1', 'E3');
+
+		$expected = new PeopleCompatibleCalculation(
+			PeopleCompatibleCalculation::ADJUSTED_WIDER, 
+			$ppc
+		);
+
+		$this->assertEquals(
+			$expected,
+			$this->transposer->calculatePeopleCompatible()
+		);
+	}
+
+	public function testPeopleCompatibleAdjustedButStillTooHigh()
+	{
+		$this->transposer->setTransposerData(
+			new NotesRange('A1', 'E3'), 
+			new NotesRange('A1', 'D3'), 
+			['Am', 'Dm'], 
+			true, 
+			new NotesRange('G#2', 'D3')
+		);
+
+		$ppc = new \NeoTransposer\Model\PeopleCompatibleTransposition($this->app);
+		$ppc->setTranspositionData(['Em', 'Am'], 5, false, 0, new NotesRange('A1', 'D3'), -1);
+		$ppc->peopleRange = new NotesRange('G#2', 'D3');
+
+		$expected = new PeopleCompatibleCalculation(
+			PeopleCompatibleCalculation::TOO_HIGH_FOR_PEOPLE, 
+			$ppc
+		);
+
+		$this->assertEquals(
+			$expected,
+			$this->transposer->calculatePeopleCompatible()
+		);
+	}
+
+	public function testPeopleCompatibleAdjustedWellHigh()
+	{
+		$this->transposer->setTransposerData(
+			new NotesRange('A1', 'E3'), 
+			new NotesRange('B1', 'B2'), 
+			['D', 'Em'], 
+			false, 
+			new NotesRange('B1', 'B2')
+		);
+
+		$ppc = new \NeoTransposer\Model\PeopleCompatibleTransposition($this->app);
+		$ppc->setTranspositionData(['D', 'Em'], 0, true, 0, new NotesRange('B1', 'B2'), -2);
+		$ppc->peopleRange = new NotesRange('B1', 'B2');
+
+		$expected = new PeopleCompatibleCalculation(
+			PeopleCompatibleCalculation::ADJUSTED_WELL, 
+			$ppc
+		);
+
+		$this->assertEquals(
+			$expected,
+			$this->transposer->calculatePeopleCompatible()
+		);
+	}
+
+	public function testPeopleCompatibleAdjustedWellLow()
+	{
+		$this->transposer->setTransposerData(
+			new NotesRange('A1', 'E3'), 
+			new NotesRange('B1', 'E3'), 
+			['Am', 'Dm', 'E'], 
+			true, 
+			new NotesRange('B1', 'F2')
+		);
+
+		$ppc = new \NeoTransposer\Model\PeopleCompatibleTransposition($this->app);
+		$ppc->setTranspositionData(['Am', 'Dm', 'E'], 0, true, 0, new NotesRange('B1', 'E3'), 1);
+		$ppc->peopleRange = new NotesRange('B1', 'F2');
+
+		$expected = new PeopleCompatibleCalculation(
+			PeopleCompatibleCalculation::ADJUSTED_WELL, 
+			$ppc
 		);
 
 		$this->assertEquals(
