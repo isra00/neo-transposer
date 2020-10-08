438da19d9bb2a6b33b31dd223a41bd4f11e0a864
Isra V <isra00@gmail.com>
2020-10-08 18:43:38 +0100

    Delete MaxMind GeoIP database from repo following privacy laws; new script to update DB according to MaxMind's new delivery system

---
+++ update_mmdb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Trying to download last MaxMind GeoLite2-Country database..."
+wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=YOURLICENSEKEY&suffix=tar.gz" -O last-mmdb.tar.gz
+tar -xf last-mmdb.tar.gz
+rm -rf last-mmdb.tar.gz
+folder=$(ls -d GeoLite2-Country_*)
+mv -f ./$folder/GeoLite2-Country.mmdb ./GeoLite2-Country.mmdb
+rm -rf ./$folder
+echo "Updated from version $folder\n"
