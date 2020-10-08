1918a0d102e546bad4e9b37e9c41c2dfe61f6533
Isra V <isra00@gmail.com>
2020-10-08 18:48:25 +0100

    Should not be doing this

--- update_mmdb.sh
+++ update_mmdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Trying to download last MaxMind GeoLite2-Country database..."
-wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=YOURLICENSEKEY&suffix=tar.gz" -O last-mmdb.tar.gz
+wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=zRqblLkfNX3FNxy9&suffix=tar.gz" -O last-mmdb.tar.gz
 tar -xf last-mmdb.tar.gz
 rm -rf last-mmdb.tar.gz
 folder=$(ls -d GeoLite2-Country_*)
