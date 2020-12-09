9662f35745154b06a4e6c12cd313d1d34119a6a6
Isra V <isra00@gmail.com>
2020-12-09 22:14:49 +0000

    Add missing chord on song #211

--- song_data.sql
+++ song_data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
 --
 -- Host: localhost    Database: transposer
 -- ------------------------------------------------------
--- Server version	8.0.20-0ubuntu0.20.04.1
+-- Server version	8.0.22-0ubuntu0.20.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -5332,6 +5332,7 @@ INSERT INTO `song_chord` VALUES (968,'Em',0);
 INSERT INTO `song_chord` VALUES (968,'Am',1);
 INSERT INTO `song_chord` VALUES (968,'B7',2);
 INSERT INTO `song_chord` VALUES (968,'C',3);
+INSERT INTO `song_chord` VALUES (211,'A',6);
 /*!40000 ALTER TABLE `song_chord` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -5344,4 +5345,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-23 15:41:24
+-- Dump completed on 2020-12-09 22:14:29
