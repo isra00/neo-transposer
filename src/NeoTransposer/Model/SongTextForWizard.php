b80b5a77f11875ec0ef0278b6a1e7c79d22c808f
isra00 <isra00@gmail.com>
2022-04-06 02:28:24 +0200

    Add a UT for SongTextForWizard

--- src/NeoTransposer/Model/SongTextForWizard.php
+++ src/NeoTransposer/Model/SongTextForWizard.php
@@ -7,38 +7,33 @@ namespace NeoTransposer\Model;
  */
 class SongTextForWizard
 {
-	/**
-	 * Lyrics and chord placeholders (%0, %1...), as fetched from config file.
-	 * 
-	 * @var string
-	 */
-	protected $rawText;
-
-	public function __construct($rawText)
-	{
-		$this->rawText = $rawText;
-	}
-
-	/**
-	 * Place the given chords in a song text with placeholders
-	 * 
-	 * @param  array	$chords Chords ready to be printed.
-	 * @return string         	HTML song text with chords.
-	 * 
-	 * @see    config.wizard.php
+    /**
+     * Lyrics and chord placeholders (%0, %1...), as fetched from config file.
      *
-     * @todo Refactor. Este bucle generando placeholders es absurdo, se puede ir sustituyendo por la marcha.
-	 */
-	public function getHtmlTextWithChords(array $chords): string
+     * @var string
+     */
+    protected $rawText;
+
+    public function __construct($rawText)
     {
-		$finalText = str_replace(' ', '&nbsp;', $this->rawText);
-        foreach ($chords as $i=>$chord)
-        {
+        $this->rawText = $rawText;
+    }
+
+    /**
+     * Place the given chords in a song text with placeholders
+     *
+     * @param array $chords Chords ready to be printed.
+     * @return string            HTML song text with chords.
+     *
+     * @see    config.wizard.php
+     */
+    public function getHtmlTextWithChords(array $chords): string
+    {
+        $finalText = str_replace(' ', '&nbsp;', $this->rawText);
+        foreach ($chords as $i => $chord) {
             $finalText = str_replace("%$i", strval($chord), $finalText);
         }
 
-		$finalText = nl2br($finalText);
-
-		return $finalText;
-	}
+        return nl2br($finalText, false);
+    }
 }
