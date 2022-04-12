07b0277dad79487db8d7d472b422d1cc41cc0125
isra00 <isra00@gmail.com>
2022-04-12 20:05:57 +0200

    Restructure tests/ and install phpmd as composer dependency

--- tests/phpmd/run-phpmd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#phpmd ../../src/ html cleancode,codesize,design,controversial,naming,unusedcode --reportfile phpmd.html
-phpmd ../../src/ html cleancode,codesize,design,naming,unusedcode --reportfile phpmd.html
