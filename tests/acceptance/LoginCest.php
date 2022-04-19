69ad2fc283e1c6c998839afd5d7b7cc9f78d18fd
isra00 <isra00@gmail.com>
2022-04-19 02:14:14 +0200

    A few more acceptance tests

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -7,17 +7,6 @@ use Faker\Factory;
 
 class LoginCest
 {
-    protected function givenASpanishNewUserWithManualRangeInBookPage(AcceptanceTester $I)
-    {
-        $faker = Factory::create();
-        $I->amOnPage('/es/login');
-        $I->fillField('email', $faker->email());
-        $I->click('sent');
-        $I->click('#i-know');
-        $I->selectOption("form select[name=lowest_note]", 'A1');
-        $I->selectOption("form select[name=highest_note]", 'E3');
-        $I->click('form button');
-    }
 
     public function existingUserShouldLoginAndSeeBookPage(AcceptanceTester $I)
     {
@@ -27,28 +16,12 @@ class LoginCest
         $I->seeElement('.page-book');
     }
 
-    public function newUserWithManualRangeShouldSeeEncourageFeedbackBanner(AcceptanceTester $I)
+    public function newUserShouldSeeUserVoicePage(AcceptanceTester $I)
     {
-        $this->givenASpanishNewUserWithManualRangeInBookPage($I);
-        $I->seeElement('.encourage-feedback');
-    }
-
-    public function newUserWithManualRangeShouldNotSeeEncourageFeedbackBannerAfterReportingFeedback3Times(AcceptanceTester $I)
-    {
-        $this->givenASpanishNewUserWithManualRangeInBookPage($I);
-
-        $I->click('.song-index li:nth-child(1) a');
-        $I->click('#feedback-yes');
-
-        $I->amOnPage('/cantos-camino-neocatecumenal');
-        $I->click('.song-index li:nth-child(2) a');
-        $I->click('#feedback-yes');
-
-        $I->amOnPage('/cantos-camino-neocatecumenal');
-        $I->click('.song-index li:nth-child(3) a');
-        $I->click('#feedback-yes');
-
-        $I->amOnPage('/cantos-camino-neocatecumenal');
-        $I->dontSeeElement('.encourage-feedback');
+        $faker = Factory::create();
+        $I->amOnPage('/es/login');
+        $I->fillField('email', $faker->email());
+        $I->click('sent');
+        $I->seeElement('.page-user-voice');
     }
 }
