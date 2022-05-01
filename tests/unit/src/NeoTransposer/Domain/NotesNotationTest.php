a9cdeca6a4753644547f026b42350f1e217cc3dc
isra00 <isra00@gmail.com>
2022-05-01 13:49:34 +0200

    Upgrade mock creation to new PHPUnit API (eventually to be replaced by COdeception mocking)

--- tests/unit/src/NeoTransposer/Domain/NotesNotationTest.php
+++ tests/unit/src/NeoTransposer/Domain/NotesNotationTest.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection ReturnTypeCanBeDeclaredInspection */
 
 namespace NeoTransposer\Tests\Domain;
 
@@ -6,49 +7,44 @@ use NeoTransposer\Domain\NotesNotation;
 
 class NotesNotationTest extends \PHPUnit\Framework\TestCase
 {
-	/**
-	 * @var NotesNotation
-	 */
-	protected $notesNotation;
+    /**
+     * @var NotesNotation
+     */
+    protected $notesNotation;
 
-	public function setUp() : void
-	{
-		$this->notesNotation = new NotesNotation();
-	}
+    public function setUp(): void
+    {
+        $this->notesNotation = new NotesNotation();
+    }
 
-	public function testGetNotation()
+    public function testGetNotation()
     {
         $this->assertEquals('Do', $this->notesNotation->getNotation('C', 'latin'));
     }
 
     public function testGetVoiceRangeAsString()
     {
-        $transMock = $this->getMockBuilder(\Symfony\Component\Translation\Translator::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['trans'])
-            ->getMock();
+        $transMock = $this->createMock(\Symfony\Component\Translation\Translator::class);
 
         $transMock->expects($this->once())
             ->method('trans')
             ->with($this->equalTo('oct'))
-            ->will($this->returnValue('octave'));
+            ->willReturn('octave');
 
         $this->assertEquals(
             'A &rarr; A +1 octave',
             $this->notesNotation->getVoiceRangeAsString($transMock, 'american', 'A1', 'A2')
         );
     }
+
     public function testGetVoiceRangeAsStringLatinNotation()
     {
-        $transMock = $this->getMockBuilder(\Symfony\Component\Translation\Translator::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['trans'])
-            ->getMock();
+        $transMock = $this->createMock(\Symfony\Component\Translation\Translator::class);
 
         $transMock->expects($this->once())
             ->method('trans')
             ->with($this->equalTo('oct'))
-            ->will($this->returnValue('octave'));
+            ->willReturn('octave');
 
         $this->assertEquals(
             'La &rarr; La +1 octave',
