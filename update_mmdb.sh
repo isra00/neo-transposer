6a1cd9ed2e39bb841f5e9fdf8cd5496e67e47c9e
isra00 <isra00@gmail.com>
2022-05-02 20:23:02 +0200

    Build: download fresh GeoIP MaxMind DB in build time

--- update_mmdb.sh
+++ update_mmdb.sh
@@ -2,7 +2,7 @@
 
 echo "Trying to download last MaxMind GeoLite2-Country database..."
 wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${NT_MAXMIND_LICENSE_KEY}&suffix=tar.gz" -O last-mmdb.tar.gz
-tar -xf last-mmdb.tar.gz
+tar xfz last-mmdb.tar.gz
 rm -rf last-mmdb.tar.gz
 folder=$(ls -d GeoLite2-Country_*)
 mv -f ./$folder/GeoLite2-Country.mmdb ./GeoLite2-Country.mmdb
