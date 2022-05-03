5a80698203038a5b1cd396ed2512cb52712a66f1
isra00 <isra00@gmail.com>
2022-05-03 13:40:08 +0200

    Update MMDB: fail if needed env var not set

--- update_mmdb.sh
+++ update_mmdb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$NT_MAXMIND_LICENSE_KEY" ]; then
+  echo "Environment variable NT_MAXMIND_LICENSE_KEY must be set before calling this script" >&2
+  exit 1
+fi
+
 echo "Trying to download last MaxMind GeoLite2-Country database..."
 wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${NT_MAXMIND_LICENSE_KEY}&suffix=tar.gz" -O last-mmdb.tar.gz
 tar xfz last-mmdb.tar.gz
