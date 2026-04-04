eee34e577b14b32969facabd2dc064d5f57f91fc
Israel Viana <israel.viana@easypark.net>
2026-04-04 18:41:33 +0200

    Implement Commitment Page

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -24,4 +24,77 @@ class LoginCest
         $I->click('sent');
         $I->seeElement('.page-user-voice');
     }
+
+    public function shouldRejectEmailWithoutAtSymbol(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', 'invalidemail');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmailWithoutDomain(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', 'test@');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmailWithoutLocalPart(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', '@example.com');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmailWithSpaces(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', 'test @example.com');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmptyEmail(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', '');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmailWithInvalidCharacters(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', 'test<>@example.com');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldRejectEmailWithoutTopLevelDomain(AcceptanceTester $I)
+    {
+        $I->amOnPage('/es/login');
+        $I->fillField('email', 'test@domain');
+        $I->click('sent');
+        $I->see('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-login');
+    }
+
+    public function shouldAcceptValidEmail(AcceptanceTester $I)
+    {
+        $faker = Factory::create();
+        $I->amOnPage('/es/login');
+        $I->fillField('email', $faker->email());
+        $I->click('sent');
+        $I->dontSee('That e-mail doesn\'t look good. Please, re-type it.');
+        $I->seeElement('.page-user-voice');
+    }
 }
