8e89cfa962a2c7d8aa5d1665c44bc61779ccd4f9
Israel Viana <israel.viana@easypark.net>
2026-04-05 13:50:16 +0200

    Fix brittle e2e test case existingUserShouldLoginAndSeeBookPage

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -11,8 +11,21 @@ class LoginCest
 
     public function existingUserShouldLoginAndSeeBookPage(AcceptanceTester $I)
     {
+        $faker = Factory::create();
+        $email = $faker->email();
+
+        // Register a new user and set voice range
         $I->amOnPage('/es/login');
-        $I->fillField('email','isra00@gmail.com');
+        $I->fillField('email', $email);
+        $I->click('sent');
+        $I->click('#i-know');
+        $I->selectOption("form select[name=lowest_note]", 'A1');
+        $I->selectOption("form select[name=highest_note]", 'E3');
+        $I->click('form button');
+
+        // Log back in with the same email
+        $I->amOnPage('/es/login');
+        $I->fillField('email', $email);
         $I->click('sent');
         $I->seeElement('.page-book');
     }
