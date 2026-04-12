fbc4cf8a2c1ec9e95e5bed2b7b2ed41e1f0bb3ca
Israel Viana <israel.viana@easypark.net>
2026-04-12 23:40:42 +0200

    Migrate wizard empiric highest, lowest & final step

---
+++ app/Http/Controllers/WizardEmpiricController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use NeoTransposer\Domain\AutomaticTransposer;
+use NeoTransposer\Domain\NotesCalculator;
+use NeoTransposer\Domain\Repository\BookRepository;
+use NeoTransposer\Domain\Repository\UserRepository;
+use NeoTransposer\Domain\Service\UnhappinessManager;
+use NeoTransposer\Domain\SongTextForWizard;
+use NeoTransposer\Domain\TransposedSong;
+use NeoTransposer\Domain\Entity\User;
+
+/**
+ * Wizard Empiric: measure the user's voice range through an empirical test.
+ */
+final class WizardEmpiricController extends Controller
+{
+    public function __construct(
+        private readonly NotesCalculator $nc,
+        private readonly UserRepository $userRepository,
+        private readonly UnhappinessManager $unhappinessManager,
+        private readonly BookRepository $bookRepository,
+    ) {
+    }
+
+    public function lowest(Request $request)
+    {
+        $locale = app()->getLocale();
+        $wizardConfig = config('nt.voice_wizard');
+
+        if (!isset($wizardConfig[$locale]['lowest'])) {
+            /** @todo Add HTTP error code */
+            return response()->view('error', [
+                'page_title'  => __('Error'),
+                'error_title' => __('Sorry, the voice measure wizard is not available in ' . config('nt.languages')[$locale]['name']),
+            ]);
+        }
+
+        $user = session('user');
+
+        // This should not happen, as user should come from selecting a standard range.
+        if (empty($user->range)) {
+            return redirect()->route('wizard_select_standard', ['locale' => $locale]);
+        }
+
+        $action_no = $action_yes = null;
+
+        // If this is the first run, the user shouldn't click NO.
+        if (null === $request->get('can_sing')) {
+            $action_no = 'lowFirstTime';
+        }
+
+        // If yes, lower down 1 semitone and retry
+        if ('yes' == $request->get('can_sing')) {
+            $user->range->lowest = $this->nc->transposeNote($user->range->lowest, -1);
+            $user->range->highest = $this->nc->transposeNote($user->range->highest, -1);
+            $user->wizard_lowest_attempts++;
+        }
+
+        // If no, we recover the previous highest note as the singer's highest note and redirect to Highest Note test wizard.
+        if ('no' == $request->get('can_sing')) {
+            $user->range->lowest = $this->nc->transposeNote($user->range->lowest, +1);
+            $user->range->highest = $this->nc->transposeNote($user->range->highest, +1);
+
+            return redirect()->route('wizard_empiric_highest', ['locale' => $locale]);
+        }
+
+        // If too low, next "yes" won't work as usual
+        if ('C1' == $user->range->lowest) {
+            $action_yes = 'tooLow';
+        }
+
+        $tpl = $this->prepareSongForTest('lowest', AutomaticTransposer::FORCE_LOWEST);
+
+        return response()->view('wizard_empiric_lowest', array_merge($tpl, [
+            'page_title' => __('Voice measure wizard'),
+            'page_class' => 'wizard-empiric',
+            'action_yes' => $action_yes,
+            'action_no'  => $action_no,
+        ]));
+    }
+
+    public function highest(Request $request)
+    {
+        $user = session('user');
+        $action_no = $action_yes = null;
+
+        // If yes, raise up 1 semitone and retry
+        if ('yes' == $request->get('can_sing')) {
+            $user->range->highest = $this->nc->transposeNote($user->range->highest, +1);
+            $user->wizard_highest_attempts++;
+        }
+
+        // If user clicks "yes" many times, he/she will reach the highest possible note (B4)!
+        if (end($this->nc->numbered_scale) == $user->range->highest) {
+            $action_yes = 'tooHigh';
+        }
+
+        // If not, we recover the last one and pass to the next step
+        // ...and if after being alerted that B4 is too much, he/she decides to continue, stop here
+        // and force B4.
+        if ('no' == $request->get('can_sing') || $user->range->highest == 'C1') {
+            $user->range->highest = $this->nc->transposeNote($user->range->highest, -1);
+            return $this->finish();
+        }
+
+        $tpl = $this->prepareSongForTest('highest', AutomaticTransposer::FORCE_HIGHEST);
+
+        return response()->view('wizard_empiric_highest', array_merge($tpl, [
+            'page_title' => __('Voice measure wizard'),
+            'page_class' => 'wizard-empiric',
+            'action_yes' => $action_yes,
+            'action_no'  => $action_no,
+        ]));
+    }
+
+    private function prepareSongForTest(string $wizardConfigSong, int $forceVoiceLimit): array
+    {
+        $locale = app()->getLocale();
+        $wizardConfig = config('nt.voice_wizard')[$locale][$wizardConfigSong];
+        $user = session('user');
+
+        $transposedSong = TransposedSong::fromDb($wizardConfig['id_song']);
+        $transposedSong->transpose($user->range, $forceVoiceLimit);
+
+        $transposedChords = $transposedSong->transpositions[0]->chordsForPrint;
+
+        $audioFile = '/static/audio/' . $wizardConfig['id_song'] . '_' . $transposedSong->transpositions[0]->offset . '.mp3';
+
+        return [
+            'song'       => (new SongTextForWizard($wizardConfig['song_contents']))->getHtmlTextWithChords($transposedChords),
+            'song_title' => $transposedSong->song->title,
+            'song_key'   => $transposedChords[0],
+            'song_capo'  => $transposedSong->transpositions[0]->getCapoForPrint(),
+            'show_audio' => config('nt.audio') && file_exists(public_path($audioFile)),
+            'audio_file' => $audioFile,
+        ];
+    }
+
+    private function finish()
+    {
+        $user = session('user');
+        $locale = app()->getLocale();
+
+        // Only when wizard is finished, voice range is stored in DB
+        $this->userRepository->saveWithVoiceChange($user, User::METHOD_WIZARD);
+
+        // If user is unhappy, UnhappyUser will consider this as an action taken.
+        $this->unhappinessManager->changedVoiceRangeFromWizard($user);
+
+        $redirectRoute = 'book_' . $this->bookRepository->readIdBookFromLocale($locale);
+        $params = ['wizardFinished' => 1];
+
+        if (!empty(session('callbackSetUserToken'))) {
+            $redirectRoute = 'external_login_finish';
+            $params['locale'] = $locale;
+        }
+
+        return redirect()->route($redirectRoute, $params);
+    }
+}
---
+++ resources/views/wizard_empiric_lowest.blade.php
@@ -0,0 +1,86 @@
+@extends('wizard_empiric_base')
+
+@section('title')@lang('Step 2: let\'s find your lowest note')@endsection
+
+@section('instructions')
+	{!! __('Try to sing the following chorus in <strong class="blink">:song_key :song_capo</strong>, with the given chords. Remember: you must sing it with low voice, but without going too low, or you won\'t be able to sing loud enough in the community!', ['song_key' => $song_key, 'song_capo' => $song_capo]) !!}
+@endsection
+
+@section('answer_buttons')
+	<button type="{{ $action_yes ? 'button' : 'submit' }}" name="can_sing" value="yes" class="big flatbutton green {{ $action_yes ?? '' }}" id="yes">@lang('Yes')</button>
+	<button type="{{ $action_no ? 'button' : 'submit' }}" name="can_sing" value="no" class="big flatbutton red {{ $action_no ?? '' }}" id="no">@lang('No, it\'s too low')</button>
+@endsection
+
+@section('messages')
+	<div class="low-first-time hidden test-msg">
+		<h3>@lang('Well, that is a bit strange')</h3>
+		<div class="inside">
+			<p>@lang('According to the voice you have chosen in the previous step, that tone should not be too low. Maybe you should go back and choose better which type of voice is yours... or maybe simply repeat the test making sure that you are singing in the same tone as the guitar.')</p>
+			<nav>
+				<a href="{{ route('wizard_step1', ['locale' => app()->getLocale()]) }}" class="big flatbutton red">&larr; @lang('Change my voice type')</a>
+				&nbsp;
+				<a href="javascript:void(0)" class="big flatbutton red" id="repeat-test">@lang('Repeat the test')</a>
+			</nav>
+		</div>
+	</div>
+
+	<div class="too-low hidden test-msg">
+		<h3>@lang('Well, that is a bit strange')</h3>
+		<div class="inside">
+			<p>@lang('If you have chosen properly your voice in the previous step and you have sung the previous attempts in the right tone with low voice, then it is practically impossible that you can sing lower than that. Maybe you should go back to the previous step and choose better your voice type:')</p>
+			<nav><a href="{{ route('wizard_step1', ['locale' => app()->getLocale()]) }}" class="big flatbutton red">&larr; @lang('Change my voice type')</a></nav>
+
+			<p>@lang('...or if you are sure that this super-low voice is yours, just go to the next step:')</p>
+			<nav><a href="{{ route('wizard_empiric_highest', ['locale' => app()->getLocale()]) }}" class="big flatbutton green" id="repeat-test">@lang('Go to the next step') &rarr;</a></nav>
+		</div>
+	</div>
+@endsection
+
+@section('wizard_scripts')
+	<script>
+	NT = {
+
+		lowFirstTime: function()
+		{
+			$(".test-area").hide();
+			$(".low-first-time").show();
+			NTSound.stopSound();
+		},
+
+		tooLow: function()
+		{
+			$(".test-area").hide();
+			$(".too-low").show();
+			gtag('event', 'LowestFirst', {'event_category': 'WizardError', 'event_label': 'user_id: {{ session('user')->id_user }}'});
+			NTSound.stopSound();
+		},
+
+		repeatTest: function()
+		{
+			document.forms[0].submit();
+		},
+
+		preventFormSubmit: function()
+		{
+			$("#form-answer").submit(function(e) {
+				e.preventDefault();
+			});
+		}
+	};
+
+	$(function()
+	{
+		$(".lowFirstTime").click(function(e) {
+			NT.preventFormSubmit();
+			NT.lowFirstTime();
+		});
+
+		$(".tooLow").click(function(e) {
+			NT.preventFormSubmit();
+			NT.tooLow();
+		});
+
+		$("#repeat-test").click(NT.repeatTest);
+	});
+	</script>
+@endsection
